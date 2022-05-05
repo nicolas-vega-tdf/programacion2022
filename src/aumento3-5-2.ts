2812a95431be060efdbec3cd00479922ad04cf93
nicolas-vega-tdf <104174254+nicolas-vega-tdf@users.noreply.github.com>
2022-05-05 17:53:25 -0300

    aumento3-5

--- src/aumento3-5-2.ts
+++ src/aumento3-5-2.ts
@@ -1,21 +1,8 @@
-/* 
-Una empresa desea premiar a sus empleados con un aumento de sueldo. 
-Este aumento se ajusta a la siguiente tabla:
- Sueldo Actual  Sueldo con Aumento
-0 - 15.000 $                    20%
-15.001 - 20.000  $          10%
-20.001 - 25.000  $           5%
-Más de 25.000 $       No hay aumento
- Desarrolle un algoritmo dado el salario actual de un empleado 
- determine el aumento de sueldo a aplicar y se lo muestre
-Recuerde plantear el Pseudocódigo y las Pruebas de Escritorio
-console.log("el preciofinal es", precioproducto);
-*/
-
 let sueldo1: number = Number(prompt("ingres salario"));
+let aumento: boolean = true;
 let salario: number = 0;
 
-switch (true) {
+switch (aumento) {
   case 0 <= sueldo1 && sueldo1 <= 15000:
     salario = sueldo1 * 1.2;
     console.log("el salario actual es:", salario);
