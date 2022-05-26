d3d205a49b3b1510372e4adb62ae533064bf7c31
nicolas-vega-tdf <104174254+nicolas-vega-tdf@users.noreply.github.com>
2022-05-26 12:34:03 -0300

    corrección sin terminar

--- src/clase24-5entrega.ts
+++ src/clase24-5entrega.ts
@@ -10,44 +10,36 @@ let alumnos: string[] = new Array(long);
 let notas1: number[] = new Array(long);
 let notas2: number[] = new Array(long);
 let notas3: number[] = new Array(long);
+let encontrado: boolean = false;
 let sumaNotas: number = 0;
+let indice = 0;
 
-for (let indice = 0; indice <= long; indice++) {
+for (let indice = 0; indice < alumnos.length; indice++) {
   alumnos[indice] = prompt("ingrese nombre de los alumos");
 }
 
-for (let i = 0; i <= long; i++) {
+for (let i = 0; i < notas1.length; i++) {
   notas1[i] = Number(prompt("ingrese las notas (primer trimestre) "));
 }
 
-for (let i = 0; i <= long; i++) {
+for (let i = 0; i < notas2.length; i++) {
   notas2[i] = Number(prompt("ingrese las notas (segundo trimestre) "));
 }
 
-for (let i = 0; i <= long; i++) {
+for (let i = 0; i < notas3.length; i++) {
   notas3[i] = Number(prompt("ingrese las notas (tercer trimestre) "));
 }
 
-for (let i = 0; i <= long; i++) {
-  sumaNotas = (sumaNotas + notas1[i] + notas2[i] + notas3[i]) / 3;
+let buscarAlum: string = prompt(" nombre del alumno a buscar ");
+while (indice < long && !encontrado) {
+  if (alumnos[indice] === buscarAlum) {
+    encontrado = true;
+    sumaNotas = (notas1[indice] + notas2[indice] + notas3[indice]) / 3;
+  }
+  indice++;
 }
-
-console.log(alumnos + " su promedio anual es: " + sumaNotas);
-
-// function notastrimestre(){
-//   for(let i = 0;i<=long;i++){
-//     notas1[i]=Number(prompt("ingrese las notas (primer trimestre) "))
-//   }
-// }
-
-// let long: number = Number(prompt("ingrese cantidad de alumnos"))
-// let alumnos : string[] = new Array(long)
-// let notas1 : number[] = new Array(long)
-
-// for(let indice = 0;indice <= long;indice++){
-//   alumnos[indice]=prompt("ingrese nombre de los alumos");
-// }
-
-// notastrimestre()
-// notastrimestre()
-// notastrimestre()
+if (encontrado) {
+  console.log(alumnos + " su promedio anual es: " + sumaNotas);
+} else {
+  console.log("No se pudo encontrar a " + buscarAlum);
+}
